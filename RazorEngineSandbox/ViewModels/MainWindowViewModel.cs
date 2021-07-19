98e9c78e62fb57afd1cafc0ebbd30c8656d21a76
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2021-07-19 12:54:40 +0200

    Implemented fody

--- RazorEngineSandbox/ViewModels/MainWindowViewModel.cs
+++ RazorEngineSandbox/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,13 @@
 ﻿namespace RazorEngineSandbox.ViewModels
 {
-  using System.ComponentModel;
-
   using Microsoft.Practices.Unity;
-
   using Prism.Events;
   using Prism.Mvvm;
-
   using RazorEngineSandbox.Data;
   using RazorEngineSandbox.Events;
   using RazorEngineSandbox.Properties;
   using RazorEngineSandbox.Services;
+  using System.ComponentModel;
 
   internal class MainWindowViewModel : BindableBase
   {
@@ -18,22 +15,7 @@
 
     private readonly TemplateService templateService;
 
-    /// <summary>The <see cref="Code" /> property's value.</summary>
-    private string propCode;
-
-    /// <summary>The <see cref="CodeDiagnostics" /> property's value.</summary>
-    private string propCodeDiagnostics;
-
-    /// <summary>The <see cref="Result" /> property's value.</summary>
-    private string propResult;
-
-    /// <summary>The <see cref="Template" /> property's value.</summary>
-    private string propTemplate;
-
-    /// <summary>The <see cref="TemplateDiagnostics" /> property's value.</summary>
-    private string propTemplateDiagnostics;
-
-    #endregion
+    #endregion Fields
 
     #region Constructors
 
@@ -48,50 +30,32 @@
       var eventAggregator = container.Resolve<IEventAggregator>();
       eventAggregator.GetEvent<ParseCompleted>().Subscribe(this.HandleParseCompletedEvent, ThreadOption.UIThread);
 
-      this.Code = "new { Name = \"World\" }";
+      this.Input = "new { Name = \"World\" }";
       this.Template = Resources.Template;
     }
 
-    #endregion
+    #endregion Constructors
 
     #region Properties
 
-    /// <summary>Gets or sets the code.</summary>
-    public string Code
-    {
-      get { return this.propCode; }
-      set { this.SetProperty(ref this.propCode, value); }
-    }
-
     /// <summary>Gets or sets the code diagnostics info.</summary>
-    public string CodeDiagnostics
-    {
-      get { return this.propCodeDiagnostics; }
-      set { this.SetProperty(ref this.propCodeDiagnostics, value); }
-    }
+    public string CodeDiagnostics { get; set; }
+
+    /// <summary>Gets or sets the input.</summary>
+    public string Input { get; set; }
+
+    public string Output { get; set; }
 
     /// <summary>Gets or sets the result.</summary>
-    public string Result
-    {
-      get { return this.propResult; }
-      set { this.SetProperty(ref this.propResult, value); }
-    }
+    public string Result { get; set; }
 
     /// <summary>Gets or sets the template.</summary>
-    public string Template
-    {
-      get { return this.propTemplate; }
-      set { this.SetProperty(ref this.propTemplate, value); }
-    }
+    public string Template { get; set; }
 
     /// <summary>Gets or sets the templateDiagnostics.</summary>
-    public string TemplateDiagnostics
-    {
-      get { return this.propTemplateDiagnostics; }
-      set { this.SetProperty(ref this.propTemplateDiagnostics, value); }
-    }
+    public string TemplateDiagnostics { get; set; }
 
-    #endregion
+    #endregion Properties
 
     #region Methods
 
@@ -109,12 +73,12 @@
     /// <param name="e">The <see cref="System.ComponentModel.PropertyChangedEventArgs" /> instance containing the event data.</param>
     private void MainWindowViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
     {
-      if (e.PropertyName == nameof(MainWindowViewModel.Code) || e.PropertyName == nameof(MainWindowViewModel.Template))
+      if (e.PropertyName == nameof(MainWindowViewModel.Input) || e.PropertyName == nameof(MainWindowViewModel.Template))
       {
-        this.templateService.Parse(this.Code, this.Template);
+        this.templateService.Parse(this.Input, this.Template);
       }
     }
 
-    #endregion
+    #endregion Methods
   }
 }
